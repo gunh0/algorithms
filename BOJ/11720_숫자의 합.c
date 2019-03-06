498500d9047049f7f54ae36199cc3ac3e5166c26
Gunho Park <devgun@kakao.com>
2019-03-06 20:28:41 +0900

    Upload

---
+++ BOJ/11720_숫자의 합.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int N, result = 0, i;
+	scanf("%d", &N); getchar();	//개행을 읽기 위해 넣음.
+	for (i = 0; i < N; i++)
+		result += getchar() - '0';
+	printf("%d\n", result);
+}
