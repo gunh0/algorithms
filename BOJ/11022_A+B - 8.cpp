fdb68aca7ea4413adb96f2c0c3572e827335840a
Gunho Park <devgun@kakao.com>
2019-08-10 13:19:31 +0900

    commit

---
+++ BOJ/11022_A+B - 8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t = 0;
+	cin >> t;
+	int a = 0, b = 0;
+	for (int i = 1; i <= t; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << '\n';
+	}
+	return 0;
+}
