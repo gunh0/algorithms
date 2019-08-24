459146e4afca13f7eea8197987534d590412ea76
Gunho Park <devgun@kakao.com>
2019-08-24 15:12:03 +0900

    commit

---
+++ BOJ/11365_!밀비 급일.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str="";
+	do
+	{
+		if (str != "")
+		{
+			for (int i = str.size() - 1; i >= 0; i--)
+				cout << str[i];
+			cout << endl;
+		}
+		getline(cin, str);
+	} while (str != "END");
+	return 0;
+}
