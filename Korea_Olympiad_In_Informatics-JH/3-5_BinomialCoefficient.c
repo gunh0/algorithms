74c1bc509166504c42384f72539e41355017e347
Gunho Park <devgun@kakao.com>
2019-09-20 16:48:07 +0900

    commit

--- Korea_Olympiad_In_Informatics-JH/3-5_BinomialCoefficient.c
+++ Korea_Olympiad_In_Informatics-JH/3-5_BinomialCoefficient.c
@@ -10,10 +10,22 @@ int main()
 	int i = 0, j = 0;
 	int n = 0, r = 0;
 	int b[6][4];
-	int min=0;
+	int min = 0;
 
 	n = 5;	// 다섯명의 사람
 	r = 3;	// 3명의 대표
+
+	for (i = 0; i <= n; i++)
+	{
+		min = (i < r) ? i : r;
+		for (j = 0; j <= min; j++)
+		{
+			if (j == 0 || j == i)b[i][j] = 1;
+			else b[i][j] = b[i - 1][j - 1] + b[i - 1][j];
+		}
+	}
+
+	printf("%d", b[n][r]);
 
 	return 0;
 }
