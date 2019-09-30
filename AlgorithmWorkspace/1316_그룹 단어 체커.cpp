aeca7bc86df73b928386906df172c671884448b8
Gunho Park <devgun@kakao.com>
2019-09-30 12:34:18 +0900

    commit

--- AlgorithmWorkspace/1316_그룹 단어 체커.cpp
+++ AlgorithmWorkspace/1316_그룹 단어 체커.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
+	int n, cnt = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		bool alphabet[26] = { 0 }, overlap = false; // false
+		char input[100];
+		int aryNum;
+		cin >> input;
+		for (int j = 0; input[j]; j++)
+		{
+			if (input[j] < 91) aryNum = input[j] - 65;
+			else aryNum = input[j] - 97;
 
+			if (alphabet[aryNum])
+			{
+				if (input[j - 1] != input[j])
+				{
+					overlap = true;
+					break;
+				}
+			}
+			else
+				alphabet[aryNum] = true;
+
+		}
+		if (!overlap) cnt++;
+	}
+	cout << cnt << endl;
 	return 0;
 }
