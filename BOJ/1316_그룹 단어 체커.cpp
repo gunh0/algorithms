745121eb0810b34cc7f2b544285c2e7bdd0496f5
Gunho Park <devgun@kakao.com>
2019-10-05 23:06:19 +0900

    commit

--- BOJ/1316_그룹 단어 체커.cpp
+++ BOJ/1316_그룹 단어 체커.cpp
@@ -7,18 +7,18 @@ int main()
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		bool alphabet[26] = { 0 }, overlap = false; // false
-		char input[100];
+		bool alphabet[26] = { 0, }, overlap = false;
+		string input;
 		int aryNum;
 		cin >> input;
 		for (int j = 0; input[j]; j++)
 		{
-			if (input[j] < 91) aryNum = input[j] - 65;
-			else aryNum = input[j] - 97;
+			if (input[j] <= 'Z') aryNum = input[j] - 'A';
+			else aryNum = input[j] - 'a';
 
 			if (alphabet[aryNum])
 			{
-				if (input[j - 1] != input[j])
+				if (input[j - 1] != input[j])	// check before(input[j-1]) & now(input[j])
 				{
 					overlap = true;
 					break;
@@ -26,10 +26,10 @@ int main()
 			}
 			else
 				alphabet[aryNum] = true;
-
 		}
-		if (!overlap) cnt++;
+		if (!overlap) cnt++;	// is group word
 	}
+
 	cout << cnt << endl;
 	return 0;
 }
