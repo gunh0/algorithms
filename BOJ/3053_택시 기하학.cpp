dfe85367bee40c69da12a631add279fcdfb3d805
Gunho Park <devgun@kakao.com>
2019-12-29 12:13:30 +0900

    commit

---
+++ BOJ/3053_택시 기하학.cpp
@@ -0,0 +1,14 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double R, S1, S2;
+    std::cin >> R;
+    S1 = M_PI * R * R;
+    S2 = R * R * 2;
+
+    printf("%.6f\n", S1);
+    printf("%.6f\n", S2);
+    return 0;
+}
