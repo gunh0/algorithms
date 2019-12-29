949ba9c2ab43d888170d39ec61944442d15164ad
Gunho Park <devgun@kakao.com>
2019-12-30 02:30:49 +0900

    commit

---
+++ BOJ/2737_피보나치 수.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> vec;
+	vec.push_back(0);
+	vec.push_back(1);
+	vec.push_back(1);
+
+	int n;
+	cin >> n;
+	if (n >= 3) {
+		for (int i = 3; i <= n; i++)
+			vec.push_back(vec[i - 2] + vec[i - 1]);
+	}
+	cout << vec[n] << endl;
+	return 0;
+}
