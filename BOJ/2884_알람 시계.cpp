82eca7f4477b13d1983ce5292e0df86b2db830ed
Gunho Park <devgun@kakao.com>
2020-01-21 13:33:25 +0900

    modify 2884

--- BOJ/2884_알람 시계.cpp
+++ BOJ/2884_알람 시계.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
-
 using namespace std;
+
+int H, M;
+
+void Earlier()
+{
+	int hour, minute;
+	if (M < 45) {
+		minute = (60 + M - 45) % 60;
+		hour = H - 1;
+		// hour 이 0일 경우에서 1시간 빼는경우 23으로 가야한다.
+		if (hour < 0)
+			hour = 23;
+	}
+	else
+	{
+		minute = M - 45;
+		hour = H;
+	}
+	cout << hour << " " << minute << endl;
+}
 
 int main()
 {
-	int h = 0, m = 0;
-	cin >> h >> m;
-	m -= 45;
-	if (m < 0)
-	{
-		m += 60;
-		h -= 1;
-		if (h <= 0)
-		{
-			h = 24 + h;
-		}
-	}
-	cout << h << " " << m << endl;
-
+	cin >> H >> M;
+	Earlier();
 	return 0;
 }
