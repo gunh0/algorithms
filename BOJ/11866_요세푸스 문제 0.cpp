3f1bc8107f65aff11685dc7de50fa35cb7c12820
Gunho Park <devgun@kakao.com>
2020-01-28 21:57:08 +0900

    BOJ 11866

---
+++ BOJ/11866_요세푸스 문제 0.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	int* arr = new int[N] {0, };
+	int* order = new int[N];
+	int point = -1;
+	for (size_t i = 0; i < N; i++) {
+		for (size_t j = 0; j < K; j++) {
+			if (arr[(++point) % N] != 0)
+				j--;
+		}
+		arr[point % N] = 1;
+		order[i] = point % N + 1;
+	}
+	cout << '<' << order[0];
+	for (size_t i = 1; i < N; i++)
+		cout << ", " << order[i];
+	cout << '>';
+	return 0;
+}
