ce49c812f47a0027b01e8a8ed18098238aa554c2
Gunho Park <devgun@kakao.com>
2020-01-29 16:17:21 +0900

    BOJ 14681

---
+++ BOJ/14681_사분면 고르기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 0, y = 0;
+	cin >> x >> y;
+
+	if (x > 0 && y > 0) cout << 1 << endl;
+	else if (x < 0 && y>0) cout << 2 << endl;
+	else if (x < 0 && y < 0) cout << 3 << endl;
+	else cout << 4 << endl;
+
+	return 0;
+}
