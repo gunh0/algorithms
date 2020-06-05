d735e0a64efbb33cfbdf6eca466603615e0d8073
Gunho Park <devgun@kakao.com>
2020-06-06 02:10:52 +0900

    BOJ 2293

---
+++ BOJ/2293_동전 1.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, k;
+	int coins[101];
+	int d[10001] = { 0 };
+
+	scanf("%d %d", &n, &k);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &coins[i]);
+	}
+	d[0] = 1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j >= coins[i]) {
+				d[j] += d[j - coins[i]];
+			}
+		}
+	}
+
+	printf("%d\n", d[k]);
+
+	return 0;
+}
