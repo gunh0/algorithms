6d97a21e5e221f2ef71ceee569b939e33d65d1d4
Gunho Park <devgun@kakao.com>
2020-08-18 22:25:36 +0900

    BOJ 1644

---
+++ BOJ/1644_소수의 연속합.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	vector<int> primes;
+	vector<bool> check;
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	check.resize(n + 1, true);
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (!check[i]) continue;
+		for (int j = i * i; j <= n; j += i)
+			check[j] = false;
+	}
+	for (int i = 2; i <= n; i++)
+		if (check[i]) primes.push_back(i);
+
+	int result = 0, sum = 0, lo = 0, hi = 0;
+
+	while (1)
+	{
+		if (sum >= n) {
+			sum -= primes[lo++];
+		}
+		else if (hi == primes.size()) {
+			break;
+		}
+		else {
+			sum += primes[hi++];
+		}
+
+		if (sum == n) result++;
+	}
+
+	cout << result;
+	return 0;
+}
