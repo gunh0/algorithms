31ff8fbba2c1b94d93cb844a0a337c12bc92fa24
Gunho Park <devgun@kakao.com>
2021-02-27 16:25:00 +0900

    BOJ 24262

---
+++ BOJ/24262_알고리즘_수업_-_알고리즘의_수행_시간_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // 코드1의 수행 횟수는 항상 1
+    cout << "1" << endl;
+
+    // 수행 횟수를 다항식으로 표현했을 때 최고차항의 차수는 0
+    cout << "0" << endl;
+
+    return 0;
+}
